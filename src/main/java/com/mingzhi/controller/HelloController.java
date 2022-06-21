8951e6bc90a0c689f2d72b1ee2bae3b46feeb500
mingzhi <mingzhi_ml@126.com>
2022-06-21 08:38:37 +0800

    fist add all test code

---
+++ src/main/java/com/mingzhi/controller/HelloController.java
@@ -0,0 +1,17 @@
+package com.mingzhi.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ *
+ */
+@Controller
+public class HelloController {
+    @GetMapping("/hello")
+    public String hello(String name, Model model){
+        model.addAttribute("name",name);
+        return "hello";
+    }
+}
